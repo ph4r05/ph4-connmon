488ca6b91d01185088db8aa8d2b3ac5dfd682967
Dusan Klinec <dusan.klinec@gmail.com>
2023-01-30 12:34:10 +0100

    fix installation, v0.0.3

--- assets/install.sh
+++ assets/install.sh
@@ -1,15 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -ex
 
-ls /home/tunnel
+cd "${SCRIPT_DIR}"
 mkdir -p /home/tunnel/mon/
 cp config.json /home/tunnel/mon/mon-config.json
 chmod 0640 /home/tunnel/mon/mon-config.json
 chown tunnel:tunnel -R /home/tunnel/mon
 chown tunnel:tunnel /home/tunnel/mon/mon-config.json
 
-cp ph4conmon.sh /home/tunnel/mon
-chmod 0755 /home/tunnel/mon/ph4conmon.sh
+cp ph4connmon.sh /home/tunnel/mon
+chmod 0755 /home/tunnel/mon/ph4connmon.sh
 
 cp ph4connmon.service /etc/systemd/system/ph4connmon.service
 systemctl daemon-reload
